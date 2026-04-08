a1c39144281f09861698f3c525e6a643b1749e5d
Faheem <ST10476752@rcconnect.edu.za>
2026-04-08 16:26:07 +0200

    Added voice greeting

---
+++ Audioplayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Media;
+using System;
+
+public class AudioPlayer
+{
+    public static void PlayGreeting()
+    {
+        try
+        {
+            SoundPlayer player = new SoundPlayer("greeting.wav");
+            player.PlaySync();
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("🔇 (Audio file missing)");
+        }
+    }
+}
